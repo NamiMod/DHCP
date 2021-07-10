96a3dfbd9d8f770cc16e82f5fb09486e58374690
S.NamiModarressi <S.NamiModarressi@gmail.com>
2021-07-10 22:00:35 +0430

    discovery and request added

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -1,8 +1,6 @@
 package com.company;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
 import java.util.Random;
 
@@ -14,7 +12,7 @@ public class Client {
 
         long start;
         long end;
-        String ip = null;
+        String ip = "";
         int time = initial_interval;
 
         while(true) {
@@ -31,10 +29,13 @@ public class Client {
                     start = System.currentTimeMillis();
                     DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                     clientSocket.receive(receivePacket);
+                    DataInputStream din = new DataInputStream(new ByteArrayInputStream(receiveData));
                     end = System.currentTimeMillis();
-                    ip = getIP(receiveData);
-                    if (timeForGetOffer(start, end , time) && ip != null) {
-                        break;
+                    if (timeForGetOffer(start, end , time)) {
+                        ip = getIP(din);
+                        if (ip != null) {
+                            break;
+                        }
                     } else {
                         time = updateTime(time);
                     }
@@ -45,8 +46,9 @@ public class Client {
                 start = System.currentTimeMillis();
                 DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                 clientSocket.receive(receivePacket);
+                DataInputStream din = new DataInputStream(new ByteArrayInputStream(receiveData));
                 end = System.currentTimeMillis();
-                if (getAck(receiveData) && end-time <= TimeOut * 1000L) {
+                if (end-start <= TimeOut * 1000L && getAck(din)) {
                     break;
                 }
             }
@@ -97,28 +99,103 @@ public class Client {
         outStream.writeByte(0x00); // GIADDR
         outStream.writeByte(0x00); // GIADDR
 
-        outStream.writeByte(0x00); // GIADDR
-        outStream.writeByte(0x00); // GIADDR
-        outStream.writeByte(0x00); // GIADDR
-        outStream.writeByte(0x00); // GIADDR
+        outStream.writeByte(0x00); // CHADDR 1
+        outStream.writeByte(0x05); // CHADDR 1
+        outStream.writeByte(0x3C); // CHADDR 1
+        outStream.writeByte(0x04); // CHADDR 1
 
+        outStream.writeByte(0x8D); // CHADDR 2
+        outStream.writeByte(0x59); // CHADDR 2
+        outStream.writeByte(0x00); // CHADDR 2
+        outStream.writeByte(0x00); // CHADDR 2
 
+        outStream.writeByte(0x00); // CHADDR 3
+        outStream.writeByte(0x00); // CHADDR 3
+        outStream.writeByte(0x00); // CHADDR 3
+        outStream.writeByte(0x00); // CHADDR 3
+
+        outStream.writeByte(0x00); // CHADDR 4
+        outStream.writeByte(0x00); // CHADDR 4
+        outStream.writeByte(0x00); // CHADDR 4
+        outStream.writeByte(0x02); // CHADDR 4
+
+        outStream.writeByte(0x01); // OPTION
 
         return out.toByteArray();
     }
-    public static byte[] createRequestMessage(){
+    public static byte[] createRequestMessage() throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        DataOutputStream outStream = new DataOutputStream(out);
+
+        outStream.writeByte(0x01); // op
+
+        outStream.writeByte(0x01); // HTYPE
+
+        outStream.writeByte(0x06); // HLEN
+
+        outStream.writeByte(0x00); // HOPS
+        //ToDo : random XID
+        outStream.writeByte(0x01); // XID
+        outStream.writeByte(0x02); // XID
+        outStream.writeByte(0x03); // XID
+        outStream.writeByte(0x04); // XID
+
+        outStream.writeByte(0x00); // SECS
+        outStream.writeByte(0x00); // SECS
+
+        outStream.writeByte(0x00); // FLAGS
+        outStream.writeByte(0x00); // FLAGS
+
+        outStream.writeByte(0x00); // CIADDR
+        outStream.writeByte(0x00); // CIADDR
+        outStream.writeByte(0x00); // CIADDR
+        outStream.writeByte(0x00); // CIADDR
+
+        outStream.writeByte(0x00); // YIADDR
+        outStream.writeByte(0x00); // YIADDR
+        outStream.writeByte(0x00); // YIADDR
+        outStream.writeByte(0x00); // YIADDR
+
+        outStream.writeByte(0x00); // SIADDR
+        outStream.writeByte(0x00); // SIADDR
+        outStream.writeByte(0x00); // SIADDR
+        outStream.writeByte(0x00); // SIADDR
+
+        outStream.writeByte(0x00); // GIADDR
+        outStream.writeByte(0x00); // GIADDR
+        outStream.writeByte(0x00); // GIADDR
+        outStream.writeByte(0x00); // GIADDR
+
+        outStream.writeByte(0x00); // CHADDR 1
+        outStream.writeByte(0x05); // CHADDR 1
+        outStream.writeByte(0x3C); // CHADDR 1
+        outStream.writeByte(0x04); // CHADDR 1
+
+        outStream.writeByte(0x8D); // CHADDR 2
+        outStream.writeByte(0x59); // CHADDR 2
+        outStream.writeByte(0x00); // CHADDR 2
+        outStream.writeByte(0x00); // CHADDR 2
+
+        outStream.writeByte(0x00); // CHADDR 3
+        outStream.writeByte(0x00); // CHADDR 3
+        outStream.writeByte(0x00); // CHADDR 3
+        outStream.writeByte(0x00); // CHADDR 3
+
+        outStream.writeByte(0x00); // CHADDR 4
+        outStream.writeByte(0x00); // CHADDR 4
+        outStream.writeByte(0x00); // CHADDR 4
+        outStream.writeByte(0x00); // CHADDR 4
+
+        outStream.writeByte(0x03); // OPTION
+
+        return out.toByteArray();
+    }
+    public static String getIP(DataInputStream din){
         return null;
     }
-
-    public static String getIP(byte[] offer){ return null; }
-    public static boolean getAck(byte[] offer){
+    public static boolean getAck(DataInputStream din){
         return false;
     }
-
-
-
-
-
     public static boolean timeForGetOffer(long start , long end , int time){
         long temp = end - start ;
         if (temp < time*1000L){
@@ -131,6 +208,5 @@ public class Client {
         int temp = (int)(time * 2 * rand.nextDouble());
         return Math.min(temp, backoff_cutoff);
     }
-
 
 }
